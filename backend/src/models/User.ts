a418a4488dde20788d2d9e7897ab44d5d5a629cd
Thanos Papakostas <thanapapas@gmail.com>
2025-10-30 17:40:38 +0200

    User Model added.

---
+++ backend/src/models/User.ts
@@ -0,0 +1,21 @@
+import mongoose, {Document, Schema} from 'mongoose';
+
+export interface IUser extends Document {
+    username: string;
+    email: string;
+    password: string;
+    isAdmin: boolean;
+}
+
+const UserSchema: Schema<IUser> = new Schema(
+    {
+    username: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    isAdmin: {type: Boolean, default: false},
+
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model<IUser>("User", UserSchema);
