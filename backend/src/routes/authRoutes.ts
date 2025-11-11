1e41fef7b1f6b898cbe8231f7a4940e2dfe01fe7
Thanos Papakostas <thanapapas@gmail.com>
2025-11-11 15:40:28 +0200

    feat: validate and OpenAPi

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,10 +1,62 @@
 import { Router } from "express";
 import { register, login } from "../controllers/authController";
+import { validate } from "../middleware/validate";
+import {registerSchema, loginSchema} from "../validation/authSchemas";
+
 
 const router = Router();
 
-router.post("/register", register);
-router.post("/login", login);
+/**
+ * @openapi
+ * /api/auth/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/RegisterRequest'
+ *     responses:
+ *       201:
+ *         description: Registered successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/AuthResponse'
+ *       400:
+ *         description: Validation error
+ */
+router.post("/register",validate(registerSchema), register);
+
+
+/**
+ * @openapi
+ * /api/auth/login:
+ *   post:
+ *     summary: Login
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/LoginRequest'
+ *     responses:
+ *       200:
+ *         description: Login successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/AuthResponse'
+ *       400:
+ *         description: Validation error
+ *       401:
+ *         description: Invalid credentials
+ */
+router.post("/login", validate(loginSchema), login);
+
 
 export default router;
 
