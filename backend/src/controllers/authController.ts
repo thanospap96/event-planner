25c8f203f768112ed8e31187289a4d9498b84446
Thanos Papakostas <thanapapas@gmail.com>
2025-11-11 15:38:21 +0200

    Update

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 import * as authService from "../services/authService"
+import {RegisterData, LoginData} from "../types/authTypes";
 
-export const register = async (req: Request, res: Response) => {
+export const register = async (req: Request<{}, {}, RegisterData>, res: Response) => {
     try {
         const result = await authService.registerUser(req.body);
         res.status(201).json(result);
@@ -10,12 +11,12 @@ export const register = async (req: Request, res: Response) => {
     }
 }
 
-export const login = async (req: Request, res: Response) => {
+export const login = async (req: Request<{}, {}, LoginData>, res: Response) => {
     try {
         const result = await authService.loginUser(req.body);
         res.status(200).json(result);
     } catch(err: any) {
-        res.status(400).json({message: err.message});
+        res.status(400).json({message: "User not found" });
     }
 
 };
