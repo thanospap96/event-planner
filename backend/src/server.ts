ec768260ef23170a1127f5c252d4e63766a3277d
Thanos Papakostas <thanapapas@gmail.com>
2025-11-11 15:45:25 +0200

    feat: swagger

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,22 +4,23 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import authRoutes from './routes/authRoutes';
 import userRoutes from './routes/userRoutes';
+import {setupSwagger} from "./docs/swagger";
 
 dotenv.config();
 const app = express();
 
+
 app.use(cors());
 app.use(express.json());
-
+setupSwagger(app)
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
-// app.use("/api/auth", authRoutes)
-//
-// app.get('/', (req, res) => {
-//     res.status(200).send('Welcome Event-Planner!');
-// });
+
+
 app.get("/api/health", (_req, res) => res.json({ ok: true }));
+
+
 mongoose
     .connect(process.env.MONGO_URI!)
     .then(() => {
