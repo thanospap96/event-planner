da46595553c398936e6b4e692d0c40beb5f2fc97
Thanos Papakostas <thanapapas@gmail.com>
2025-11-14 17:24:35 +0200

    feat: event types

---
+++ backend/src/types/eventTypes.ts
@@ -0,0 +1,26 @@
+export interface EventResponse {
+    id: string;
+    title: string;
+    description?: string;
+    date: String;
+    location?: string;
+    capacity?: number;
+    createdBy: string;
+    attendees: string[];
+}
+
+export interface CreateEventData {
+    title: string;
+    description?: string;
+    date: string;
+    location?: string;
+    capacity?: number;
+}
+
+export interface UpdateEventData {
+    title?: string;
+    description?: string;
+    date?: string;
+    location?: string;
+    capacity?: number;
+}
