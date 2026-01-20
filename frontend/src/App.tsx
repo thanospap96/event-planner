1c89fe45f694822b29c6b844522560f1e32de6d9
Thanos Papakostas <thanapapas@gmail.com>
2026-01-20 18:40:10 +0200

    NavBar n Outlet fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,36 @@
-import {Route, Routes} from "react-router-dom";
-import EventList from "./components/pages/EventList.tsx";
-import CreateEvent from "./components/pages/CreateEvent.tsx";
-import EventDetail from "./components/pages/EventDetail.tsx";
-import ProtectedRoute from "./routes/ProtectedRoute.tsx";
-import {Navigate} from "react-router-dom";
-import WelcomePage from "./components/pages/WelcomePage.tsx";
-import NavBar from "./components/NavBar.tsx";
-import Footer from "./components/layout/Footer.tsx";
-import { useAuth } from "./context/AuthContext.tsx";
-import { SearchProvider } from "./context/SearchContext.tsx";
+import { Route, Routes, Navigate, Outlet } from "react-router-dom";
+import EventList from "./components/pages/EventList";
+import CreateEvent from "./components/pages/CreateEvent";
+import EventDetail from "./components/pages/EventDetail";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import WelcomePage from "./components/pages/WelcomePage";
+import NavBar from "./components/NavBar";
+import Footer from "./components/layout/Footer";
+import { SearchProvider } from "./context/SearchContext";
 
 export default function App() {
-    const { token } = useAuth();
-    const isAuthenticated = token !== null;
-
     return (
-        <SearchProvider>                         
-                {isAuthenticated && <NavBar />}
-                
-                    <Routes>
-                        <Route path="/" element={<WelcomePage />} />
-
-                        {/* Protected */}
-                        <Route element={<ProtectedRoute />}>
-                            <Route path="/events" element={<EventList/>} />
-                            <Route path="/events/new" element={<CreateEvent/>} />
-                            <Route path="/events/:id" element={<EventDetail/>} />
-                        </Route>
-
-                        <Route path="*" element={<Navigate to="/" />} />
-                    </Routes>
-                
-                {isAuthenticated && <Footer />}            
+        <SearchProvider>
+            <Routes>
+                <Route path="/" element={<WelcomePage />} />
+                {/* Protected */}
+                <Route element={<ProtectedRoute />}>
+                    <Route
+                        element={
+                            <>
+                                <NavBar />
+                                <Outlet />
+                                <Footer />
+                            </>
+                        }
+                    >
+                        <Route path="/events" element={<EventList />} />
+                        <Route path="/events/new" element={<CreateEvent />} />
+                        <Route path="/events/:id" element={<EventDetail />} />
+                    </Route>
+                </Route>
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
         </SearchProvider>
-    )
+    );
 }
